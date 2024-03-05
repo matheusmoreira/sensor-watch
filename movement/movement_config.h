899bc340453f36032784268fb47a82b434c70df1
Matheus Afonso Martins Moreira <matheus.a.m.moreira@gmail.com>
2024-03-05 05:18:18 -0300

    custom: configure watch faces
    Add a full set of meter and timer complications.
    Add TOTP, RNG, tallying functionality.
    Add time, interval and deadline counters.
    Replace the time setter with the hackwatch time setter.
    Add the precision fine tuning watch faces.

--- movement/movement_config.h
+++ movement/movement_config.h
@@ -29,14 +29,25 @@
 
 const watch_face_t watch_faces[] = {
     clock_face,
+    pulsometer_face,
+    ratemeter_face,
+    stopwatch_face,
+    timer_face,
+    probability_face,
+    tally_face,
+    day_one_face,
+
     world_clock_face,
+    totp_face,
+    tachymeter_face,
+    thermistor_readout_face,
     sunrise_sunset_face,
     moon_phase_face,
-    stopwatch_face,
+    voltage_face,
     preferences_face,
-    set_time_face,
-    thermistor_readout_face,
-    voltage_face
+    set_time_hackwatch_face,
+    finetune_face,
+    nanosec_face,
 };
 
 #define MOVEMENT_NUM_FACES (sizeof(watch_faces) / sizeof(watch_face_t))
@@ -47,7 +58,7 @@ const watch_face_t watch_faces[] = {
  * Some folks also like to use this to hide the preferences and time set faces from the normal rotation.
  * If you don't want any faces to be excluded, set this to 0 and a long Mode press will have no effect.
  */
-#define MOVEMENT_SECONDARY_FACE_INDEX (MOVEMENT_NUM_FACES - 2) // or (0)
+#define MOVEMENT_SECONDARY_FACE_INDEX (MOVEMENT_NUM_FACES - 11) // or (0)
 
 /* Custom hourly chime tune. Check movement_custom_signal_tunes.h for options. */
 #define SIGNAL_TUNE_DEFAULT
